b775b85e0e7cfee4719ae00e172f7f304addd661
Robert Beyer <robert.beyer@uni-rostock.de>
2021-07-26 13:14:01 +0200

    valSplit

--- python/functions/groupData.py
+++ python/functions/groupData.py
@@ -32,3 +32,13 @@ def groupData(inputData,outputData,val_split):
     outputDataValid  = outputData[round(outputData.shape[0]*val_split):outputData.shape[0]]
     
     return inputDataTrain,inputDataValid,outputDataTrain,outputDataValid
+
+
+
+# def groupData(inputData,outputData,val_split):
+#     inputDataTrain = inputData[0:round(inputData.shape[0]*(1-val_split))]
+#     inputDataValid  = inputData[round(inputData.shape[0]*(1-val_split)):inputData.shape[0]]
+#     outputDataTrain = outputData[0:round(outputData.shape[0]*(1-val_split))]
+#     outputDataValid  = outputData[round(outputData.shape[0]*(1-val_split)):outputData.shape[0]]
+    
+#     return inputDataTrain,inputDataValid,outputDataTrain,outputDataValid
