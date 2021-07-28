fb393cb2ec7141016770118e8495e6ea577677d4
Robert Beyer <robert.beyer@uni-rostock.de>
2021-07-28 18:08:14 +0200

    new group data

--- python/functions/groupData.py
+++ python/functions/groupData.py
@@ -24,21 +24,10 @@ Function for grouping a input/output dataset into train and valid data
     -----
     
 """
-
 def groupData(inputData,outputData,val_split):
-    inputDataTrain = inputData[0:round(inputData.shape[0]*val_split)]
-    inputDataValid  = inputData[round(inputData.shape[0]*val_split):inputData.shape[0]]
-    outputDataTrain = outputData[0:round(outputData.shape[0]*val_split)]
-    outputDataValid  = outputData[round(outputData.shape[0]*val_split):outputData.shape[0]]
+    inputDataTrain = inputData[0:round(inputData.shape[0]*(1-val_split))]
+    inputDataValid  = inputData[round(inputData.shape[0]*(1-val_split)):inputData.shape[0]]
+    outputDataTrain = outputData[0:round(outputData.shape[0]*(1-val_split))]
+    outputDataValid  = outputData[round(outputData.shape[0]*(1-val_split)):outputData.shape[0]]
     
     return inputDataTrain,inputDataValid,outputDataTrain,outputDataValid
-
-
-
-# def groupData(inputData,outputData,val_split):
-#     inputDataTrain = inputData[0:round(inputData.shape[0]*(1-val_split))]
-#     inputDataValid  = inputData[round(inputData.shape[0]*(1-val_split)):inputData.shape[0]]
-#     outputDataTrain = outputData[0:round(outputData.shape[0]*(1-val_split))]
-#     outputDataValid  = outputData[round(outputData.shape[0]*(1-val_split)):outputData.shape[0]]
-    
-#     return inputDataTrain,inputDataValid,outputDataTrain,outputDataValid
